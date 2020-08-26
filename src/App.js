c9f93f85322fce39134c830aa5cc28a848311d3d
yirano <yirano@protonmail.ch>
2020-08-26 12:53:25 -0400

    Save merge

--- src/App.js
+++ src/App.js
@@ -5,53 +5,29 @@ import Header from './components/Header/Header'
 import { Switch, Route } from 'react-router-dom'
 import PrivateRoute from './utils/PrivateRoute'
 
-<<<<<<< HEAD
 import Home from './components/Home/Home'
 import SignIn from './components/SignIn/SignIn'
 import SignUp from './components/SignUp/SignUp'
 import Articles from './components/Articles/Articles'
 import Profile from './components/Profile/Profile'
+import NewArticle from './components/NewArticle/NewArticle'
+import ArticleEditor from './components/ArticleEditor/ArticleEditor'
 
-export default function App() {
+function App() {
 	return (
 		<div className="App">
 			<Header />
-			{/* <Route exact path="/"> */}
-			<Home />
-			{/* </Route> */}
+			<Route exact path="/">
+				<Home />
+			</Route>
 			<Switch>
 				<Route path="/signin" component={SignIn} />
 				<Route path="/signup" component={SignUp} />
-				<PrivateRoute path="/profile" component={Profile} />
-				<PrivateRoute path="/articles" component={Articles} />
+				<PrivateRoute exact path="/profile" component={Profile} />
+				<PrivateRoute exact path="/add-new-article" component={NewArticle} />
+				<PrivateRoute exact path="/articles" component={Articles} />
+				<PrivateRoute exact path="/article-editor" component={ArticleEditor} />
 			</Switch>
 		</div>
 	)
-=======
-import Home from "./components/Home/Home";
-import SignIn from "./components/SignIn/SignIn";
-import SignUp from "./components/SignUp/SignUp";
-import Articles from "./components/Articles/Articles";
-import Profile from "./components/Profile/Profile";
-import NewArticle from "./components/NewArticle/NewArticle";
-import ArticleEditor from "./components/ArticleEditor/ArticleEditor";
-
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Route exact path="/">
-        <Home />
-        </Route>
-      <Switch>
-        <Route path="/signin" component={SignIn} />
-        <Route path="/signup" component={SignUp} />
-        <PrivateRoute exact path="/profile" component={Profile} />
-        <PrivateRoute exact path="/add-new-article" component={NewArticle} />
-        <PrivateRoute exact path="/articles" component={Articles} />
-        <PrivateRoute exact path="/article-editor" component={ArticleEditor} />
-      </Switch>
-    </div>
-  );
->>>>>>> 3075c29a20d633ff2011db1dcdf27a7661bba241
 }
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,11 @@
-<<<<<<< HEAD
 import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
+import { fetchArticles } from '../../redux/actions/articlesActions'
 
 const Home = (props) => {
-	const { something } = props
-	console.log('SOMETHING', something)
-	console.log('PROPS', props)
+	useEffect(() => {
+		props.fetchArticles()
+	}, [])
 
 	return (
 		<div>
@@ -16,33 +16,8 @@ const Home = (props) => {
 
 const mapStateToProps = (state) => {
 	return {
-		something: state.articlesReducer.message
+		message: state.message
 	}
 }
 
-export default connect(mapStateToProps, {})(Home)
-=======
-import React, { useEffect } from "react";
-import { connect } from "react-redux";
-import { fetchArticles } from "../../redux/actions/articlesActions";
-
-const Home = (props) => {
-  useEffect(() => {
-    props.fetchArticles();
-  }, []);
-
-  return (
-    <div>
-      <h1>HOME</h1>
-    </div>
-  );
-};
-
-const mapStateToProps = (state) => {
-  return {
-    message: state.message,
-  };
-};
-
-export default connect(mapStateToProps, { fetchArticles })(Home);
->>>>>>> 3075c29a20d633ff2011db1dcdf27a7661bba241
+export default connect(mapStateToProps, { fetchArticles })(Home)
